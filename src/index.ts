bdb897e20ab5773f16c984b51e8ee73fdb517e54
anirudh1713 <anirudhatodaria11@gmail.com>
2022-01-16 19:24:39 +0530

    add eslint with basic express server :wrench:

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Prabandhak');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server started on port 3001 🚀');
+});
