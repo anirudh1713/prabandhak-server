f6b4a2c752b67c98e66f84d9604a7c267701f588
anirudh1713 <anirudhatodaria11@gmail.com>
2022-01-20 18:46:01 +0530

    setup cors, helmet :lock:

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-const express = require('express');
-
-const app = express();
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server started on port 3001 🚀');
+  console.log(`Server started on port ${PORT} 🚀`);
 });
