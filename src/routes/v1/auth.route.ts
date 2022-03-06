ae4ae8bb3d0698e9160e69b4454c6b8c21a22a43
anirudh1713 <anirudhatodaria11@gmail.com>
2022-03-06 18:18:14 +0530

    :recycle: refactor validation middleware

--- src/routes/v1/auth.route.ts
+++ src/routes/v1/auth.route.ts
@@ -1,26 +1,7 @@
 import express from 'express';
-import { z } from 'zod';
+import { validate } from '../../middlewares/validate';
 import * as authValidation from '../../validations/auth.validation';
-
-const zErrorMap: z.ZodErrorMap = (issue, ctx) => {
-  switch (issue.code) {
-    case 'invalid_type':
-      return { message: `${issue.path} must be of type ${issue.expected}` };
-
-    default:
-      return { message: ctx.defaultError };
-  }
-};
 
 export const authRouter = express.Router();
 
-authRouter.post('/register', (req, res, next) => {
-  try {
-    authValidation.register.parse(req.body, { errorMap: zErrorMap });
-    next();
-  } catch (error: any) {
-    // eslint-disable-next-line no-console
-    console.log(error.message);
-    // Trow new APIError here.
-  }
-});
+authRouter.post('/register', validate(authValidation.register));
--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
 export const register = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
+  body: z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  }),
 });
 
 export const login = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  body: z.object({
+    email: z.string().email(),
+    password: z.string(),
+  }),
 });
